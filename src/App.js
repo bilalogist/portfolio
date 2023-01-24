2787aba476f91daafe886e50a5e67a7e8d1d0def
Muhammad Bilal Nasir <bilalogist@gmail.com>
2023-01-25 00:17:34 +0500

    init

---
+++ src/App.js
@@ -0,0 +1,221 @@
+
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,AiFillGithub,AiFillMail
+} from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { useState } from "react";
+import dp from "./assets/DP.jpeg";
+import code from "./assets/code.png";
+import design from "./assets/design.png";
+import consulting from "./assets/consulting.png";
+import web1 from "./assets/web1.png";
+import web2 from "./assets/web2.png";
+import web3 from "./assets/web3.png";
+import web4 from "./assets/web4.png";
+import web5 from "./assets/web5.png";
+import web6 from "./assets/web6.png";
+import one from "./assets/one.png";
+import { Wave } from "react-animated-text";
+
+export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <div className={darkMode ? "dark" : ""}>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">@bilalogist</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="/resume.pdf" target={"_blank"}
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              Muhammad Bilal Nasir
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              <Wave iterations={1} speed={20} text="Web Developer" />
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              I am a Software Engineer with a great passion for software
+              development who specializes in creating web applications using the
+              MERN Stack.
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <AiFillLinkedin
+                onClick={() =>
+                  window.open("https://linkedin.com/in/bilalogist", "blank")
+                }
+              />
+              <AiFillGithub
+                onClick={() =>
+                  window.open("https://github.com/bilalogist", "blank")
+                }
+              />
+              <AiFillMail
+                onClick={() =>
+                  window.open("mailto://bilalogist@gmail.com", "blank")
+                }
+              />
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <img src={dp} layout="fill" objectFit="cover" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
+              <img src={design} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2  ">Frontend</h3>
+              <p className="py-2">
+                Creating elegant frontend to suited for your needs integrating
+                with backend apis as well.
+              </p>
+              <h4 className="py-4 text-teal-600">Tools / Languages</h4>
+              <p className="text-gray-800 py-1">React JS</p>
+              <p className="text-gray-800 py-1">Typescript</p>
+              <p className="text-gray-800 py-1">Material UI/ Bootstrap</p>
+              <p className="text-gray-800 py-1">Redux</p>
+              <p className="text-gray-800 py-1">CSS/SCSS</p>
+              <p className="text-gray-800 py-1">VS Code</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <img src={code} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">
+                Backend / Fullstack
+              </h3>
+              <p className="py-2">
+                Do you have an idea for your next great website? Let's make it a
+                reality.
+              </p>
+              <h4 className="py-4 text-teal-600">Tools / Languages</h4>
+              <p className="text-gray-800 py-1">Node JS</p>
+              <p className="text-gray-800 py-1">Express JS</p>
+              <p className="text-gray-800 py-1">React JS</p>
+              <p className="text-gray-800 py-1">Typescript</p>
+              <p className="text-gray-800 py-1">Mongo DB</p>
+              <p className="text-gray-800 py-1">SQL</p>
+            </div>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <img src={consulting} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
+              <p className="py-2">
+                Are you interested in feedback for your current project? I can
+                give you tips and tricks to level it up.
+              </p>
+              <h4 className="py-4 text-teal-600">Tools / Languages</h4>
+              <p className="text-gray-800 py-1">React JS</p>
+              <p className="text-gray-800 py-1">Node JS</p>
+              <p className="text-gray-800 py-1">FIGMA / Adobe XD</p>
+            </div>
+          </div>
+        </section>
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a freelance designer and
+              developer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including brand design,
+              programming and teaching.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1 ">
+              <img
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={one}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web2}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web3}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web4}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web5}
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <img
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+                src={web6}
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
